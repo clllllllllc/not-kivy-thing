294fc75ab583ef9fa0b8184676ee60a95f91af5e
clllllllllc <clllllllllc@protonmail.com>
2022-07-13 22:25:24 +1000

    lol

--- grug/__main__.py
+++ grug/__main__.py
@@ -12,6 +12,7 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.pagelayout import PageLayout
 from kivy.properties import StringProperty
 from kivy.uix.textinput import TextInput
+from kivymd.app import MDApp
 
 import pandas as pd
 
@@ -30,19 +31,19 @@ class LoginScreen(Screen):
 
     def check_login(self):
         users_url = r"grug/saves/users.json"
-        user_dict = dict()
         with open(users_url) as json_file:
             users_dict = json.loads(json_file.read())
             print(users_dict)
         login = False
+        print(users_dict)
         # Add code
         for i in users_dict["users"]:
-            print(i["username"])
-            print(i["password"])
-            print(self.temp_pass)
-            print(self.temp_user)
-            if i["username"] == self.temp_user and i["password"] == self.temp_pass:
-                self.login = True
+            username = self.ids.username.text
+            password = self.ids.password.text
+            print(username, password)
+            print(i["username"], i["password"])
+            if i["username"] == username and i["password"] == password:
+                login = True
                 self.login_id = i["user_id"]
 
         if login:
@@ -58,27 +59,68 @@ class SignupScreen(Screen):
 
     def check_signup(self):
         users_url = r"grug/saves/users.json"
-        users_dict = json.loads(users_url)
+        with open(users_url) as json_file:
+            users_dict = json.loads(json_file.read())
+            print(users_dict)
         newacc = True
         # Add code
-        for i in users_dict:
-            if i["username"] == self.temp_user or i["email"] == self.temp_email:
+        for i in users_dict["users"]:
+            username = self.ids.username.text
+            password = self.ids.password.text
+            email = self.ids.email.text
+            code = self.ids.code.text
+
+            if i["username"] == username or i["email"] == email:
                 newacc = False
 
         if newacc:
+            new_user = {
+                "user_id": 1,
+                "username": username,
+                "password": password,
+                "email": email,
+                "rank": "none",
+                "servers": [],
+                "settings": {
+                    "button_font": "Default",
+                    "button_color": "Default",
+                    "button_bgcolor": "Default",
+                    "label_font": "Default",
+                    "label_color": "Default",
+                    "font_size": "Default",
+                    "bg_image": "Default",
+                    "window_size": "Default"
+                }
+            }
+            users_dict["users"].append(new_user)
+            json_object = json.dumps(users_dict, indent = 4)
+            with open(users_url, "w") as json_file:
+                json_file.write(json_object)
             self.manager.current = "menu"
 
 
 class UserScreen(Screen):
-    pass
+
+    def add_server(self):
+        pass
+    
+    def on_click_server(self):
+        pass
+    def on_click_thing(self):
+        pass
+    
+    def on_click_settings(self):
+        pass
 
 
 class SettingsScreen(Screen):
     pass
 
 
-class GrugApp(App):
+class GrugApp(MDApp, App):
     def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Red"
         sm = ScreenManager()
         sm.add_widget(MenuScreen(name="menu"))
         sm.add_widget(LoginScreen(name="login"))
